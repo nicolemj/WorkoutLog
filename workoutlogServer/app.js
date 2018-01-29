49507f7a54ab1aa109af0540cf6e2b62eb15c548
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-01-29 11:33:34 -0500

    Module 5

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(require('./middleware/headers'));
 
 app.use('/api/test', function(req, res){
     res.send("Hello World")
-})
+});
 
 
 app.listen(3000, function(){

61fe0930332502301a896a765fd5b50293c7273c
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-01-30 05:27:54 -0500

    Module 11

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
-
 var User = sequelize.import(__dirname + '\\models\\user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
 app.use(bodyParser.json());
+app.use('/api/login', require('./routes/session'));
 app.use(require('./middleware/headers'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/test', function(req, res){
@@ -19,7 +19,7 @@ app.listen(3000, function(){
 });
 
 
-
+app.use(bodyParser.json());
 
 
 
--- workoutlogServer/routes/user.js
+++ workoutlogServer/routes/user.js
@@ -1,24 +1,24 @@
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
-
+var bcrypt = require('bcryptjs');
 
 router.post('/', function(req, res) {
 		var username = req.body.user.username;
 		var pass = req.body.user.password;
-		//Need to create a user object and use sequelize to put that user into
-		//
 
 		User.create({
 			username: username,
-			passwordhash: pass
+			passwordhash: bcrypt.hashSync(pass, 10)
 		}).then(
 		//Sequelize is going to return the object it created from db.
 
 			function createSuccess(user){
+                var token = jwt.sign({id:user.id}, 'i_am_secret', {expireIn:60*60*24})
 				res.json({
 						user: user,
-						message: 'create'
+                        message: 'create',
+                        sessionToken: token
 				});
 			},
 			function createError(err){
